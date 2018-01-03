16578434892bb4143a605f3ac81c2678c0b67984
nancy sin <nsin@sbcglobal.net>
2018-01-03 02:33:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+const kittens = ["Milo", "Otis", " Garfield"]
+
+function appendKitten (name){
+  return [...kittens, name];
+}
+
+function destructivelyAppendKitten (name){
+   return kittens.push(name);
+}
+ function prependKitten(name){
+   return [name, ...kittens];
+ }
+
+ function destructivelyPrePendKitten(name){
+   return kittens.unshift(name);
+ }
+
+ function removeLastKitten(){
+   return kittens.slice(0, kittens.length-1);
+ }
+
+ function destructivelyRemoveLastKitten(){
+   kittens.pop();
+   return kittens;
+ }
+
+ function removeFirstKitten(){
+    newkittens= kittens.slice(1);
+    return newkittens
+ }
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
